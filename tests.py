998e6780bb10ab258abfd731b802175efc2d5c44
David Mears <kieukmail@gmail.com>
2019-08-23 11:36:37 +0100

    Add .gitignore and setUp method

--- tests.py
+++ tests.py
@@ -3,9 +3,11 @@ from module import file
 
 class ModuleTests(unittest.TestCase):
 
+    def setUp(self):
+        self._object = file.ObjectClass()
+
     def test_can_create_instances(self):
-        _object = file.ObjectClass()
-        self.assertTrue(_object)
+        self.assertTrue(self._object)
 
 if __name__ == "__main__":
     unittest.main()
